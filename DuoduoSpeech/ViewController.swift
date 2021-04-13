6ba162b9e3084ada9cb5ca1a390549056d64636b
fanchangyong <fanchangyong@gmail.com>
2021-04-13 12:32:12 +0800

    Initial commit

---
+++ DuoduoSpeech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuoduoSpeech
+//
+//  Created by fancy on 4/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
